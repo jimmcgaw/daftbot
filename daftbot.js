e02d139f9edbc2971fba3bcbbaeae2ab7ea626fb
Jim McGaw <jpmcgaw@gmail.com>
2014-05-15 00:38:19 -0700

    tests and adding vehicle class

--- daftbot.js
+++ daftbot.js
@@ -72,6 +72,159 @@ var gpio = require('pi-gpio');
   daftbot.Whisker = Whisker;
 
 
+  // car brain - this commands and manages the motors in aggregate
+  // @param Array 'motors' - array of Motor objects
+  // @param Array 'whiskers' - array of Whisker objects
+  //
+  var Vehicle = function(motors, sensors){
+    this.motors = [];
+    this.sensors = [];
+
+    this.leftMotor = motors.leftMotor;
+    this.rightMotor = motors.rightMotor;
+
+    this.addMotor(this.leftMotor);
+    this.addMotor(this.rightMotor);
+
+    this.state = "forward";
+  };
+
+  Vehicle.prototype.addMotor = function(motor){
+    this.motors.push(motor);
+  };
+
+  Vehicle.prototype.addSensor = function(sensor){
+    this.sensors.push(sensor);
+  };
+
+  Vehicle.prototype.forward = function(){
+    for (var i = 0, len = this.motors.length; i < len; i++){
+      this.motors[i].forward();
+    }
+  };
+
+  Vehicle.prototype.reverse = function(){
+    for (var i = 0, len = this.motors.length; i < len; i++){
+      this.motors[i].reverse();
+    }
+  };
+
+  Vehicle.prototype.halt = function(){
+    for (var i = 0, len = this.motors.length; i < len; i++){ 
+      this.motors[i].halt();
+    } 
+  };
+
+  Vehicle.prototype.right = function(){
+    this.halt();
+    this.leftMotor.forward();
+    this.rightMotor.reverse();
+  };
+
+  Vehicle.prototype.left = function(){
+    this.halt();
+    this.rightMotor.forward();
+    this.leftMotor.reverse();
+  };
+
+  Vehicle.prototype.rotate = function(){
+      this.left();
+  };
+
+
+  // enables all motors and sensors
+  Vehicle.prototype.startEngine = function(){
+    for (var i = 0, len = this.motors.length; i < len; i++){
+      this.motors[i].enable();
+    }
+    for (var i = 0, len = this.sensors.length; i < len; i++){
+      this.sensors[i].enable();
+    }
+  };
+
+  Vehicle.prototype.getWhiskers = function(){
+    this.sensors.filter(function(sensor){
+      return sensor instanceof Whisker;
+    });
+  };
+
+
+  Vehicle.prototype.checkWhiskers = function(){
+    // if this.state !== "forward"{
+    //   return;
+    // }
+
+    var vehicle = this;
+    var whiskers = this.getWhiskers();
+
+    for (var i = 0, len = whiskers.length; i < len; i++){
+      var whisker = whiskers[i];
+      gpio.read(whisker.pinIn, function(err, value){
+        console.log(value);
+        if (value === HIGH){
+          // we have a collision, reset
+          vehicle.reset();
+        }
+      });
+    }
+
+  };
+
+  // intiates a state sequence that resets the robot
+  // to forward in a different direction.
+  Vehicle.prototype.reset = function(){
+    var vehicle = this;
+
+    // stop robot immediately ( t = 0 seconds )
+    vehicle.state = "halt";
+
+    // t = 1 : go in reverse 
+    setTimeout(function(){
+      vehicle.state = "reverse";
+    }, 1000);
+
+    // t = 1.5 : stop again 
+    setTimeout(function(){
+      vehicle.state = "halt"
+    }, 1500);
+
+    // t = 2 : rotate
+    setTimeout(function(){
+      vehicle.state = "rotate"
+    }, 2000);
+
+    // t = 3.5 : stop again
+    setTimeout(function(){
+      vehicle.state = "halt"
+    }, 3500);
+
+    // t = 4: stop again
+    setTimeout(function(){
+      vehicle.state = "forward"
+    }, 4000);
+  };
+
+  // runs in loop; checks whiskers and turns robot if
+  // we detect a whisker collision
+  Vehicle.prototype.update = function(){
+
+    // check object state
+    if (this.state === "forward"){
+      this.forward();
+      this.checkWhiskers();
+    } else if (this.state === "halt"){
+      this.halt();
+    } else if (this.state === "reverse") {
+      this.reverse();
+    } else if (this.state === "rotate") {
+      this.rotate();
+    }
+
+  };
+
+  daftbot.Vehicle = Vehicle;
+
+
 
   module.exports = daftbot;
 }());
